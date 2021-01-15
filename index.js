76cc3edf7fafe7bda32421439bb7246e04d6de8d
corycalaway <cory.c.calaway@gmail.com>
2021-01-15 09:47:07 -0700

    added first set of questions

--- index.js
+++ index.js
@@ -17,8 +17,72 @@ const questions = () => {
                     console.log('Please enter the name of your project!')
                 }
             }
+        },
+        // project description
+        {
+            type: 'input',
+            name: 'projectDescription',
+            message: 'Provide a description of your project!',
+            validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please enter a description of your project!')
+            }
         }
-
+        },
+        // installation instructions
+        {
+            type: 'input',
+            name: 'projectInstallation',
+            message: 'Provide installation instructions!',
+            validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('Please enter installation instructions!')
+            }
+        }
+        },
+        // usage information
+        {
+            type: 'input',
+            name: 'projectUsage',
+            message: 'Provide information on how this project is used!',
+            validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please enter information on how this project is used!')
+            }
+        }
+        },
+        // contribution guidelines
+        {
+            type: 'input',
+            name: 'projectContribution',
+            message: 'Provide information on how to contribute to this project!',
+            validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('Please enter information on how to contribute to this project!')
+            }
+        }
+        },
+        // test instructions
+        {
+            type: 'input',
+            name: 'projectTestInstructions',
+            message: 'Provide information on test instructions!',
+            validate: testInput => {
+            if (testInput) {
+                return true;
+            } else {
+                console.log('Please enter information on test instructions!')
+            }
+        }
+        },
     ])
 };
 
