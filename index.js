74790734979eeba04e9507eb4f40f0da223511de
corycalaway <cory.c.calaway@gmail.com>
2021-01-15 09:17:36 -0700

    added first question for project

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-// TODO: Include packages needed for this application
+// allows use of nodes inquirer
+const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// questions to generate readme
+const questions = () => {
+    return inquirer.prompt([
+        // project title question
+        {
+            type: 'input',
+            name: 'projectTitle',
+            message: 'What is the title of your project?',
+            validate: projectInput => {
+                if (projectInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the name of your project!')
+                }
+            }
+        }
+
+    ])
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    questions();
+
+}
 
 // Function call to initialize app
 init();
